49b4a806a3b28dbbbc1d11a1363163123bc98b18
mostie <mostie@users.noreply.github.com>
2018-07-31 12:24:40 +0200

    Testen git

---
+++ test.py
@@ -0,0 +1,23 @@
+import Requests
+import lxml.html
+a=1
+b=2
+print(a+b)
+c=a+b+b
+print(c)
+print('test\n'
+        'aaa')
+d='zomaar een test'
+print(d[2:5])
+print(len(d))
+sq=[1,3,5,7]
+print(sq)
+print('sq[0:2] : ',sq[0:2])
+ls=[0,1]
+for i in range(20):
+    ls.append(ls[-1]+ls[-2])
+print(ls)
+for j in range(0,10):
+    print(d[j])
+print(d[5:10])
+print('sq[0:2] : ',sq[0:2])
